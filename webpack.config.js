b76ca1b2d23fcc037b9c6abc0fc36468c8c4a67c
steglaset <steglaset@gmail.com>
2023-03-03 18:03:50 +0300

    init: start autoguru

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+//webpack.config.js
+const path = require('path');
+const PugPlugin = require('pug-plugin');
+
+const PATHS = {
+    src: path.join(__dirname, "src"),
+};
+
+module.exports = {
+    entry: {
+        index: './src/index.pug'
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist', ),
+        filename: 'js/[name].js'
+    },
+    plugins: [
+        new PugPlugin({
+            pretty: true,
+            css: {
+                filename: 'css/[name].css'
+            }
+        })
+    ],
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: PugPlugin.loader
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: 'css-loader'
+                }, {
+                    loader: 'postcss-loader'
+                }, {
+                    loader: 'sass-loader'
+                }]
+            },
+            {
+                test: /\.css$/,
+                use:  'css-loader'
+            },
+            {
+                test: /\.(png|jpg|jpeg|ico|webp)/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[name].[ext]'
+                }
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/fonts/[name][ext][query]'
+                }
+            },
+            {
+                test: /\.(svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/icons/[name][ext][query]'
+                }
+            }
+        ]
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist')
+        }
+    },
+    stats: 'errors-only'
+};
