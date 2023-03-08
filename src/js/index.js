534b1b56bb57f4acd577e43c370f09d639dcaf3e
steglaset <steglaset@gmail.com>
2023-03-08 14:37:48 +0300

    feat: add carousels

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
 import bootstrap from 'bootstrap';
+
+//Categories
+const catButtons = document.querySelectorAll('.prices__item');
+const categories = document.querySelectorAll('.categories__li');
+function inactivate(nodeList) {
+    [...nodeList].forEach(el => {
+        el.classList.remove('active');
+    });
+}
+[...catButtons].forEach(el => {
+    el.addEventListener('click', function() {
+        inactivate(catButtons);
+        inactivate(categories);
+
+        this.classList.add('active');
+
+        const buttonFor = this.dataset.for;
+        const catToActivate = document.querySelector(buttonFor);
+        catToActivate.classList.add('active');
+    });
+})
